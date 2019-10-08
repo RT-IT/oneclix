f37970ba4280f90eb90996fef94874220fc7fa8f
Andrew A. Barber <andrew.barber@racetrackpitstop.co.uk>
2019-10-08 10:37:11 +0100

    changed download plugin

--- wordpress-oneclix/install.sh
+++ wordpress-oneclix/install.sh
@@ -14,31 +14,31 @@ sleep 10s
 SSL="YES"
 
 while getopts d:s:ms:e option
-    do
+do
     case "${option}"
         in
-            d) DOMAIN=${OPTARG,,};;
-            s) SSL=${OPTARG^^};;
+        d) DOMAIN=${OPTARG,,};;
+        s) SSL=${OPTARG^^};;
     esac
 done
 
 if [ -z "$DOMAIN" ]; then
-        echo " "
-        echo "###########################################"
-        echo "# Pass a command flag with the domain name#"
-        echo "# Eg. install.sh -d andrewbarber.me       #"
-        echo "#   Exiting now... please try again!      #"
-        echo "###########################################"
-        exit
+    echo " "
+    echo "###########################################"
+    echo "# Pass a command flag with the domain name#"
+    echo "# Eg. install.sh -d andrewbarber.me       #"
+    echo "#   Exiting now... please try again!      #"
+    echo "###########################################"
+    exit
 fi
 if [ "$EUID" -ne 0 ]; then
-        echo " "
-        echo "###########################################"
-        echo "#          Please run with sudo           #"
-        echo "#   Exiting now... please try again!      #"
-        echo "###########################################"
-        exit
-    fi
+    echo " "
+    echo "###########################################"
+    echo "#          Please run with sudo           #"
+    echo "#   Exiting now... please try again!      #"
+    echo "###########################################"
+    exit
+fi
 
 DOMAIN="$( echo "$DOMAIN" | sed -e 's#^http://##; s#^https://##' )"
 
@@ -47,28 +47,28 @@ MYIP=$(/sbin/ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0
 /usr/bin/nslookup $DOMAIN > /tmp/nslookup.txt
 
 if ! grep -q $MYIP /tmp/nslookup.txt; then
-        echo " "
-        echo "###########################################"
-        echo "# $DOMAIN #"
-        echo "#            is not pointing to           #"
-        echo "#              $MYIP             #"
-        echo "#   Exiting now... please try again!      #"
-        echo "###########################################"
-        exit
-fi 
+    echo " "
+    echo "###########################################"
+    echo "# $DOMAIN #"
+    echo "#            is not pointing to           #"
+    echo "#              $MYIP             #"
+    echo "#   Exiting now... please try again!      #"
+    echo "###########################################"
+    exit
+fi
 
 /usr/bin/nslookup "www."$DOMAIN > /tmp/nslookup.txt
 
 if ! grep -q $MYIP /tmp/nslookup.txt; then
-        echo " "
-        echo "###########################################"
-        echo "# www.$DOMAIN #"
-        echo "#            is not pointing to           #"
-        echo "#              $MYIP             #"
-        echo "#   Exiting now... please try again!      #"
-        echo "###########################################"
-        exit
-fi 
+    echo " "
+    echo "###########################################"
+    echo "# www.$DOMAIN #"
+    echo "#            is not pointing to           #"
+    echo "#              $MYIP             #"
+    echo "#   Exiting now... please try again!      #"
+    echo "###########################################"
+    exit
+fi
 
 echo " "
 echo "###########################################"
@@ -146,8 +146,8 @@ cd /tmp/plugins/
 curl -O https://downloads.wordpress.org/plugin/webp-express.zip
 unzip /tmp/plugins/webp-express.zip -d /home/admin/web/$DOMAIN/public_html/wp-content/plugins/
 
-curl -O https://downloads.wordpress.org/plugin/wp-smushit.zip
-unzip /tmp/plugins/wp-smushit.zip -d /home/admin/web/$DOMAIN/public_html/wp-content/plugins/
+curl -O https://downloads.wordpress.org/plugin/all-in-one-wp-security-and-firewall.zip
+unzip /tmp/plugins/all-in-one-wp-security-and-firewall.zip -d /home/admin/web/$DOMAIN/public_html/wp-content/plugins/
 
 curl -O https://downloads.wordpress.org/plugin/google-analytics-for-wordpress.zip
 unzip /tmp/plugins/google-analytics-for-wordpress.zip -d /home/admin/web/$DOMAIN/public_html/wp-content/plugins/
@@ -173,10 +173,10 @@ chmod 644 /home/admin/web/$DOMAIN/public_html/wp-includes
 chmod 644 /home/admin/web/$DOMAIN/public_html/*.php
 chmod 644 /home/admin/web/$DOMAIN/public_html/*/*.php
 chmod 644 /home/admin/web/$DOMAIN/public_html/*/*/*.php
-chmod 444 /home/admin/web/$DOMAIN/public_html/wp-config.php
 chmod 444 /home/admin/web/$DOMAIN/public_html/index.php
 find /home/admin/web/$DOMAIN/public_html/ -type d -exec chmod 755 {} \;
 find /home/admin/web/$DOMAIN/public_html/ -type f -exec chmod 644 {} \;
+chmod 0640 /home/admin/web/$DOMAIN/public_html/wp-config.php
 
 sed -i -e 's/database_name_here/admin_'$DBNAME'/g' /home/admin/web/$DOMAIN/public_html/wp-config.php
 sed -i -e 's/username_here/admin_'$DBUSER'/g' /home/admin/web/$DOMAIN/public_html/wp-config.php
@@ -200,16 +200,20 @@ echo "@ini_set( 'upload_max_filesize' , '2048M' );@ini_set( 'post_max_size', '20
 echo "require_once( ABSPATH . 'wp-settings.php' );" >> /home/admin/web/$DOMAIN/public_html/wp-config.php
 
 if [ $SSL = "YES" ]; then
-
+    
     echo " "
     echo "###########################################"
     echo "#           Setting up SSL/HTTPS          #"
     echo "###########################################"
     sleep 3s
-
+    cd /usr/local/vesta/data/templates/web
+    curl -O http://c.vestacp.com/0.9.8/rhel/force-https/nginx.tar.gz
+    tar -xzvf nginx.tar.gz
+    rm -f nginx.tar.gz
+    
     /usr/local/vesta/bin/v-add-letsencrypt-domain admin $DOMAIN
     /usr/local/vesta/bin/v-change-web-domain-proxy-tpl admin $DOMAIN force-https
-
+    
 fi
 
 echo " "
@@ -217,14 +221,13 @@ echo "###########################################"
 echo "#               Cleaning Up               #"
 echo "###########################################"
 sleep 3s
-cd 
+cd
 rm -rf /tmp/wordpress/
 rm -rf /tmp/plugins/
 rm -rf /tmp/nslookup.txt
 rm -rf /tmp/db_list.txt
 
 rm -rf /home/admin/web/$DOMAIN/public_html/wp-content/plugins/hello.php
-
 
 rm -rf /home/admin/web/$DOMAIN/public_html/wp-content/themes/twentyseventeen/
 rm -rf /home/admin/web/$DOMAIN/public_html/wp-content/themes/twentysixteen/
@@ -234,6 +237,7 @@ find /home/admin/web/$DOMAIN/public_html -type d -print0 | xargs -0 -I {} find '
 
 find /home/admin/web/$DOMAIN/public_html -type d -print0 | xargs -0 -I {} find '{}' -type d -print0 | xargs -0 -I {} chmod 0755 {};
 find /home/admin/web/$DOMAIN/public_html -type d -print0 | xargs -0 -I {} find '{}' -type d -print0 | xargs -0 -I {} chown admin:admin {};
+chmod 0640 /home/admin/web/$DOMAIN/public_html/wp-config.php
 
 echo " "
 echo "###########################################"
